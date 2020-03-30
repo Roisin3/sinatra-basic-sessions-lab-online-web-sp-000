f31254b2c304383219ef2ea3f256ac4d989202fb
Michael Lynch <michael@michaelrlynch.com>
2020-03-30 20:57:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do
     enable :sessions
-    set :sessions_secret, ''
+    set :sessions_secret, 'flatiron'
+  end
+
+  get '/' do
+    erb :index
   end
 end
